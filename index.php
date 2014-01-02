5d160cdcb8bda56e297caf605237d4831943b54d
luishsilva <luizhrick@gmail.com>
2014-01-02 11:58:49 -0200

    Organizando a index.php movendo os javascripts para o final da página

--- index.php
+++ index.php
@@ -3,12 +3,11 @@
 <head>
 	<meta charset="UTF-8">
 	<link rel="stylesheet" type="text/css" href="css/style.css">
-	<script src="http://cdnjs.cloudflare.com/ajax/libs/dat-gui/0.5/dat.gui.min.js"></script>
-	<!--<script src="http://s.codepen.io/assets/libs/empty.js"></script>-->
 	<title>luishsilva</title>
 </head>
 <body>
 	<canvas id="canvas"></canvas>
 </body>
 </html>
+<script src="http://cdnjs.cloudflare.com/ajax/libs/dat-gui/0.5/dat.gui.min.js"></script>
 <script src="js/js.js" type="text/javascript"></script>
